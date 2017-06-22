fb205ccc4a9c9049e99fccabf20d8763226871fc
karangrewal <karan_gre_wal@hotmail.com>
2017-06-21 22:19:42 -0400

    training on CIFAR-10

--- data.py
+++ data.py
@@ -1,14 +1,38 @@
 import h5py
 import numpy as np
+import pickle
 
-def get_data(batch_size=32):
-    path = '<path-to-mnist-hdf5-file'
-    data = h5py.File(path)
-    data = data['features'][:]
+def get_data(batch_size=64):
+
+    # batch 1
+    with open('/u/grewalka/data-cifar-10/data_batch_1') as f:
+        data = pickle.load(f)['data']
+
+    # batch 2
+    with open('/u/grewalka/data-cifar-10/data_batch_2') as f:
+        _data = pickle.load(f)['data']
+    data = np.concatenate((data, _data), axis=0)
+
+    # batch 3
+    with open('/u/grewalka/data-cifar-10/data_batch_3') as f:
+        _data = pickle.load(f)['data']
+    data = np.concatenate((data, _data), axis=0)
+
+    # batch 4
+    with open('/u/grewalka/data-cifar-10/data_batch_4') as f:
+        _data = pickle.load(f)['data']
+    data = np.concatenate((data, _data), axis=0)
+
+    # batch 5
+    with open('/u/grewalka/data-cifar-10/data_batch_5') as f:
+        _data = pickle.load(f)['data']
+    data = np.concatenate((data, _data), axis=0)
+
+    k = data.shape[0]
+    data = data.reshape(k, 3, 32, 32)#.transpose(0, 2, 3, 1)
     data = np.float32(data)
     data = data / 255.
 
-    k = data.shape[0]
     k = k / batch_size
     k = k * batch_size
     k = data.shape[0] - k
